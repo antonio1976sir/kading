5f42a049f5eb507e73d651c4ea34f5ede807b74b
antonio1976sir <57894634+antonio1976sir@users.noreply.github.com>
2019-11-19 17:04:45 +0400

    Add files via upload

--- func.js
+++ func.js
@@ -1,34 +1,41 @@
 
 
 function readFile(input) {
-  let file = input.files[0];
-
-  let reader = new FileReader();
-
-  reader.readAsText(file);
-
-  reader.onload = function() {   
-    console.log(reader.result);
-    var arr=reader.result.split("\n");
-    console.log(arr);
-      getdata(arr);
-  };
-
-  reader.onerror = function() {
-    console.log(reader.error);
-  };
+    let file = input.files[0];
+    
+    let reader = new FileReader();
+    
+    reader.readAsText(file);
+    
+    reader.onload = function() {
+        console.log(reader.result);
+        var arr = reader.result.split("\n");
+        console.log(arr);
+        getdata(arr);
+    }
+    ;
+    
+    reader.onerror = function() {
+        console.log(reader.error);
+    }
+    ;
 
 }
 
-function getdata(arr){
-    arr.forEach(function(item){
-     $.get('http://ppoz-service-bal-01.prod.egrn:9001/#/administration/details/'+item+'/docs',function(data){
-         $.get($(data).find("div .row-data:contains('Заявление 0')").find("a").attr("href"),function(data){
-             console.log(data);
-           } //func zayav
-           ) //get zayav
-           }//get_func
-          ); //get
-    } //each func
-        ) //each
+function getdata(arr) {
+    arr.forEach(function(item) {
+        $.get('http://ppoz-service-bal-01.prod.egrn:9001/manager/requests/byId?id='+item, function(data) {
+            
+            data.files.forEach(function(item){
+            if (item.dynType=="statement") {    
+            $.get('http://ppoz-service-bal-01.prod.egrn:9001/filestorage/' + item.fileStoragePath + '?attachment=true', function(data) {
+                console.log(data);
+                        })
+            }
+            }) //foreach data.files
+        });
+        //get
+    }//each func
+    )
+    //each
 }
