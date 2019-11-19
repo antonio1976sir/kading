529dc45d716476aef5fa57be9c7d2cad9af1cd73
antonio1976sir <57894634+antonio1976sir@users.noreply.github.com>
2019-11-19 14:45:10 +0400

    Update func.js

--- func.js
+++ func.js
@@ -1,3 +1,5 @@
+
+
 function readFile(input) {
   let file = input.files[0];
 
@@ -9,10 +11,24 @@ function readFile(input) {
     console.log(reader.result);
     var arr=reader.result.split("\n");
     console.log(arr);
+      getdata(arr);
   };
 
   reader.onerror = function() {
     console.log(reader.error);
   };
 
+}
+
+function getdata(arr){
+    arr.each(function(item){
+     $.get('http://ppoz-service-bal-01.prod.egrn:9001/#/administration/details/'+item+'/docs',function(data){
+         $.get($(data).find("div .row-data:contains('Заявление 0')").find("a").attr("href"),function(data){
+             console.log(data);
+           } //func zayav
+           ) //get zayav
+           }//get_func
+          ); //get
+    } //each func
+        ) //each
 }
